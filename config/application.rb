927dc2deeff92109bca90463b4bdfffc40055ae6
User <user@otni-gmb.ng>
2025-03-03 23:56:25 -0800

    add some line in application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,15 @@
+require_relative "boot"
+
+require "rails/all"  # Make sure this is present
+
+Bundler.require(*Rails.groups)
+
 module MyApi
   class Application < Rails::Application
     config.load_defaults 8.0
 
-    # Ensure API-only mode (skips assets, helpers, etc.)
     config.api_only = true
 
-    # Include middleware for authentication if using Devise
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
   end
